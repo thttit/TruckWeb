02993272cf358a02d1dfef7289efd84cdae4c6c2
TRẦN HOÀNG THANH THY <103625040+thttit@users.noreply.github.com>
2023-05-16 14:38:40 +0700

    push

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createPool({
-  host: 'localhost',
+  host: 'group7.ueh46.com',
   port: 3307, //default: 3306
   user: 'root', //default: empty
   password: 123456,
